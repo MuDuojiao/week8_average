b9d73aa9cbc8a83672468ae4c7053406f5899e24
marilyn-814 <ucapmux@ucl.ac.uk>
2022-12-01 10:39:19 +0000

    Firs version finished by M

--- average.py
+++ average.py
@@ -1,1 +1,14 @@
 import pandas
+import numpy as np
+
+csvData = pandas.read_csv('values.csv', header=None)
+ave = []
+newCol = []
+newDF = pandas.DataFrame()
+for i in range(len(csvData.columns)):
+    newCol.append(i)
+    size = len(csvData[i])
+    ave.append(np.sum(csvData[i])/size)
+    newDF[i] = np.sum(csvData[i])/size
+resultDF = pandas.DataFrame(ave)
+resultDF.to_csv('result.csv')
