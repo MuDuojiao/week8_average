54aa33cff9a97930702cadd7b4f16f6c26ba7643
marilyn-814 <ucapmux@ucl.ac.uk>
2022-12-01 10:43:33 +0000

    Verify the input of the method

--- average.py
+++ average.py
@@ -1,14 +1,18 @@
 import pandas
 import numpy as np
 
-csvData = pandas.read_csv('values.csv', header=None)
-ave = []
-newCol = []
-newDF = pandas.DataFrame()
-for i in range(len(csvData.columns)):
-    newCol.append(i)
-    size = len(csvData[i])
-    ave.append(np.sum(csvData[i])/size)
-    newDF[i] = np.sum(csvData[i])/size
-resultDF = pandas.DataFrame(ave)
-resultDF.to_csv('result.csv')
+def getAve(path):
+    csvData = pandas.read_csv(path, header=None)
+    ave = []
+    newCol = []
+    newDF = pandas.DataFrame()
+    for i in range(len(csvData.columns)):
+        newCol.append(i)
+        size = len(csvData[i])
+        ave.append(np.sum(csvData[i])/size)
+        newDF[i] = np.sum(csvData[i])/size
+    resultDF = pandas.DataFrame(ave)
+    resultDF.to_csv('result.csv')
+
+
+getAve('./values.csv')
